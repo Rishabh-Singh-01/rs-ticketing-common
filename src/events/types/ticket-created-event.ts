ef7c683a32df01ee59e3bdc3e48d01fa361556bd
Rishabh-Singh-01 <rishabhsinghom1515@gmail.com>
2023-12-01 13:23:46 +0530

    Include order status and some minor changes

--- src/events/types/ticket-created-event.ts
+++ src/events/types/ticket-created-event.ts
@@ -1,5 +1,8 @@
 import { Subjects } from '../utils/subjects';
 
+/**
+ * Interface to define Ticket Created Event used as common module
+ */
 export interface TicketCreatedEvent {
   subject: Subjects.TicketCreated;
   data: {
