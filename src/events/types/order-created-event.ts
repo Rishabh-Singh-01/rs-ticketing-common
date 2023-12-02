f91e61863a5baf5ff7452c9433a4a9024dc87201
Rishabh-Singh-01 <rishabhsinghom1515@gmail.com>
2023-12-02 15:29:20 +0530

    Add version flag to events for occ

--- src/events/types/order-created-event.ts
+++ src/events/types/order-created-event.ts
@@ -5,7 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
-    version: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
